0d027690008cc0ff72455a29846bed7303a98ce0
enrique <roromero147@gmail.com>
2024-01-15 09:49:30 +0100

    First commit

---
+++ backend/dist/index.js
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+//conections and listeners
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log("Server Open & Connected to Database 👌"));
+})
+    .catch((err) => console.log(err));
+//# sourceMappingURL=index.js.map
